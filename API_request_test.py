64bd4aa841e1518544ea806e67603034e96a4059
Hyeongwoo Moon <moc9058@gmail.com>
2023-11-20 21:13:14 +0900

    20231120

--- API_request_test.py
+++ API_request_test.py
@@ -6,12 +6,12 @@ from multiprocessing import Process, Value, Array
 from threading import Thread
 
 import functions as func
-purpose = "自己紹介"
+purpose = "お知らせ"
 client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 completion = client.chat.completions.create(
             messages=[
-				{"role": "user", "content": f"Can you revise my professional summary?"},
-				{"role": "user", "content": f"Possess a robust academic foundation in quantitative analysis, encompassing areas such as statistics, data visualization and programming. I am seeking a challenging professional position that leverages my strong interpersonal skills, excellent time management abilities, and problem-solving skills within the public sector."}
+				{"role": "user", "content": f"次の{purpose}文を修正してください。"},
+				{"role": "user", "content": f"皆さん、こんばんは！担当実行委員、ひょんうです。重要なお知らせをご案内させていただきます。\n１)OT日の決定について\n日程上、OTを来週に進行することとなりました。それに伴い、OT参加可能時間帯の入力の締切を11月23日（木曜日）13時に設定します！急な進行ですみません。\n２)自己紹介シート作成について\nOTでは、行事の概要説明、自己紹介、そして事前学習日程の設定などが行われます。その中で、自己紹介についてのミッションがあります！OTの日まで下のシートを作成していただくことをお願いします！言語は日本語、韓国語どちらでも対応可能です。\n忙しい中、ご協力ありがとうございます！私たちも全力を尽くして本番を準備しておりますので、どうぞ楽しみにお待ちください。どうぞよろしくお願いします！"}
             ],
             model="gpt-4"
         )
